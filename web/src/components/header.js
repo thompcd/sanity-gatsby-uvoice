8ee5d272cb7219cd28ddcc33e60dba0227653ed1
Corey Thompson <corey.dwayne.thompson@gmail.com>
2019-11-09 22:28:50 -0600

    white logo, blue gradient

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import uvIcon from '../assets/images/UVOICE-logo-100x133-white.png'
 
 class Header extends React.Component {
     render() {
         return (
             <section id="header">
                 <div className="inner">
-                    <span className="icon major fa-cloud"></span>
-                    <h1>Hi, I'm <strong>Photon</strong>, another fine<br />
-                    little freebie from <a href="http://html5up.net">HTML5 UP</a>.</h1>
+                    <span className="image"><img src={uvIcon} alt="" /></span>
+                    {/* <span className="icon major fa-cloud"></span> */}
+                    <h1>Marketing strategies <br /> to help yo<strong>u</strong>r company find its <strong>voice</strong>.</h1>
                     <p>Accumsan feugiat mi commodo erat lorem ipsum, sed magna<br />
                     lobortis feugiat sapien sed etiam volutpat accumsan.</p>
                     <ul className="actions">
