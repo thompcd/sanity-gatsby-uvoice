c3392b742cd5d2fc1c5cd7b4762766eb6af2081c
Corey Thompson <corey.dwayne.thompson@gmail.com>
2020-01-08 07:16:11 -0600

    remove section image for test

--- studio/schemas/documents/pageContent.js
+++ studio/schemas/documents/pageContent.js
@@ -27,31 +27,31 @@ export default {
         title: 'Section 2 Text',
         description: 'Use images at your own risk.'
       },
-      {
-        name: 'section2Img',
-        type: 'image',
-        title: 'Section 2 Image',
-        description: 'Inline image. Will resize based on screen, focusing on hotspot.',
-        options: {
-          hotspot: true // <-- Defaults to false
-        },
-        fields: [
-          {
-            name: 'caption',
-            type: 'string',
-            title: 'Caption',
-            options: {
-              isHighlighted: true // <-- make this field easily accessible
-            }
-          },
-          {
-            // Editing this field will be hidden behind an "Edit"-button
-            name: 'attribution',
-            type: 'string',
-            title: 'Attribution',
-          }
-        ]
-      },
+      // {
+      //   name: 'section2Img',
+      //   type: 'image',
+      //   title: 'Section 2 Image',
+      //   description: 'Inline image. Will resize based on screen, focusing on hotspot.',
+      //   options: {
+      //     hotspot: true // <-- Defaults to false
+      //   },
+      //   fields: [
+      //     {
+      //       name: 'caption',
+      //       type: 'string',
+      //       title: 'Caption',
+      //       options: {
+      //         isHighlighted: true // <-- make this field easily accessible
+      //       }
+      //     },
+      //     {
+      //       // Editing this field will be hidden behind an "Edit"-button
+      //       name: 'attribution',
+      //       type: 'string',
+      //       title: 'Attribution',
+      //     }
+      //   ]
+      // },
       {
         name: 'section3Heading',
         type: 'string',
